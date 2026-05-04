c139f049b36d35e0d0d1092759d200a88c630954
József Tar <szurilo@gmail.com>
2026-05-04 14:47:56 +0200

    Remove widget script from layout
    Removed external script for widget integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
-        <script src="https://www.vibe-vaults.com/widget.js" data-key="ab8e954c712c1c1c027f707ec2c90737" async></script>
       </body>
     </html>
   )
